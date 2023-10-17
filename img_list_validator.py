9794a3342a5d149cd5ec1f8e651ae0fbe6100531
sdany <dany.sternfeld@gmail.com>
2023-10-17 22:29:46 +0300

    find all img empty records

--- img_list_validator.py
+++ img_list_validator.py
@@ -1,21 +1,33 @@
 import pyodbc
 import re
 
-def getAccessData():
-    tablepath = r'C:\Users\danys\OneDrive\Documents\TP_Shotlist1_with_timestamp.accdb'
-    tablename = 'data'
-    serialIndex = 0
-    codeIndex = 1
-    imgnumIndex = 5
-    lastnameIndex = 3
-    firstnameIndex = 4
-    timestampIndex = 7
-    Result = []
+tablename = 'data'
+serialIndex = 0
+codeIndex = 1
+imgnumIndex = 5
+lastnameIndex = 3
+firstnameIndex = 4
+timestampIndex = 7
+tablepath = r'C:\Users\danys\OneDrive\Documents\TP_Shotlist1_with_timestamp.accdb'
+
+def tableConnect():
     conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' + tablepath + ';')
-    cursor = conn.cursor()
-    cursor.execute('select * from ' + tablename)
+    cursor = conn.cursor()  
+    return cursor  
+
+def runSQL(query):
+    cursor = tableConnect()
+    cursor.execute(query)
+    return cursor.fetchall()  
+
+def getEmpty():
+    return runSQL('select * from ' + tablename + ' where פספורט is NULL')
+
+def getAccessData():
+    Result = []
+    query = 'select * from ' + tablename
     #(786, 800265.0, 'כיתה 14', 'רתם', 'קציר', '4222', None, None)   
-    for row in cursor.fetchall():
+    for row in runSQL(query):
         i = 0
         while i < len(row):
             if row[i] == None:
@@ -34,15 +46,13 @@ def ParseImgList():
     matches = pattern.findall(data[0])
     return matches
 
-
-        
-
-
+ 
 
 #################
 ### main
 #################
-#Accessdata = getAccessData()
-#print(Accessdata)
+Accessdata = getAccessData()
+print(Accessdata)
 imgList = ParseImgList()
 print(imgList)
+print(getEmpty())
