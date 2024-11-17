7e4cedcff645862f640669459f329bcada74ee97
dany <dany.sternfeld@gmail.com>
2024-11-17 12:48:24 +0200

    Cp access to NEW+ template dir

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
+copy TP_Shotlist.accdb C:\Users\danys\AppData\Local\Microsoft\PowerToys\NewPlus\Templates\NEW_TP_Shotlist.accdb
 pyinstaller -F img_list_validator.py
 copy dist\*.exe install\
 cd install
